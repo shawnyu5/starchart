6c236d6eef97685876874ebb44800008ef6f3481
shawn yu <shawn.yu5@hotmail.com>
2023-02-02 15:21:08 -0500

    change formatting

--- app/createEmotionCache.ts
+++ app/createEmotionCache.ts
@@ -1,6 +1,6 @@
 import createCache from '@emotion/cache';
 
-export const defaultCache = createEmotionCache();
+	export const defaultCache = createEmotionCache();
 
 export default function createEmotionCache() {
   return createCache({ key: 'cha' });
