7796c40c05fcb5042af8fd6666ba83867cd6630b
shawn yu <shawn.yu5@hotmail.com>
2023-02-02 15:25:16 -0500

    test

--- app/createEmotionCache.ts
+++ app/createEmotionCache.ts
@@ -1,7 +1,7 @@
 import createCache from '@emotion/cache';
 
-	export const defaultCache = createEmotionCache();
+export const defaultCache = createEmotionCache();
 
-export default function createEmotionCache() {
+export default function createEmotionCacheeee() {
   return createCache({ key: 'cha' });
 }
